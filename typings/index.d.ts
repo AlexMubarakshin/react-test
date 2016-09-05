bc87757c646fd303ab694d8a098d2399376f474a
Harlock <harlockbcn@gmail.com>
2016-09-05 20:54:43 +0200

    initial commit based on http://blog.tomduncalf.com/posts/setting-up-typescript-and-react/

---
+++ typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="require.d.ts" />
+/// <reference path="globals/react-dom/index.d.ts" />
+/// <reference path="globals/react-redux/index.d.ts" />
+/// <reference path="globals/react/index.d.ts" />
+/// <reference path="globals/redux/index.d.ts" />
+
